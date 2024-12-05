e2e576d2037fdd6cf3e6a5e5b00e3a544fa52cad
aiu149 <tianzhongzuoji2@gmail.com>
2024-12-05 11:48:38 +0900

    Update Car_Chapter15.java
    runメソッドを変更

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -27,20 +27,9 @@ public class Car_Chapter15 {
 		System.out.println();
 	}
 	
-
 	public void run() {
-		if( this.speed == 10 ) {
+		if( this.speed != 0 ) {
 			System.out.print("速度は時速"+ this.speed +"kmです");
-		} else if( this.speed == 20 ) {
-			System.out.print("速度は時速"+ this.speed +"kmです");
-		} else if( this.speed == 30 ) {
-			System.out.print("速度は時速"+ this.speed +"kmです");
-		} else if( this.speed == 40 ){
-			System.out.print("速度は時速"+ this.speed +"kmです");
-		} else if( this.speed == 50 ){
-			System.out.print("速度は時速"+ this.speed +"kmです");
-		} else {
-			System.out.print("速度は時速"+ 10 +"kmです");
 		}
 
 	}
