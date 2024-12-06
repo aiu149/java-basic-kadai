62e88d42e62a95f722929b20f8cff5decfece1a1
aiu149 <tianzhongzuoji2@gmail.com>
2024-12-06 17:10:36 +0900

    Update KatoExec_Chapter18.java
    課題を修正

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,9 +4,16 @@ public class KatoExec_Chapter18 {
 
 	public static void main(String[] args) {
 		
-		KatoTaro_Chapter18 a = new KatoTaro_Chapter18("加藤","東京都中野区");
-		a.setGivenName("太郎");
+		KatoTaro_Chapter18 a = new KatoTaro_Chapter18("加藤","太郎","東京都中野区");
+		KatoIchiro_Chapter18 b = new KatoIchiro_Chapter18("加藤","一郎","東京都中野区");
+		KatoHanako_Chapter18 c = new KatoHanako_Chapter18("加藤","花子","東京都中野区");
+		
+		a.setGivenName();
 		a.execlntroduce();
+		b.setGivenName();
+		b.execlntroduce();
+		c.setGivenName();
+		c.execlntroduce();
 		
 
 	}
