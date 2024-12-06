a5d311a41037fa481e5d0d6ded58f0bf8c88d33c
aiu149 <tianzhongzuoji2@gmail.com>
2024-12-06 17:11:29 +0900

    Update KatoHanako_Chapter18.java
    課題を修正

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -2,20 +2,18 @@ package kadai_018;
 
 public class KatoHanako_Chapter18 extends Kato_Chapter18 {
 	
-	public KatoHanako_Chapter18(String familyName,String address) {
-		super(familyName,address);
+	public KatoHanako_Chapter18(String familyName,String givenName,String address) {
+    	super(familyName,givenName,address);
 	}
+    public void setGivenName() {
 	
-    public void setGivenName(String givenName ) {
-	
-	    this.givenName = givenName;
 	    System.out.println();
 	
  }
 public void eachlntroduce() {
-	System.out.println("私の名前は加藤" + givenName + "です");
 	System.out.println("趣味は読書です");
 	
 }
 
 }
+
