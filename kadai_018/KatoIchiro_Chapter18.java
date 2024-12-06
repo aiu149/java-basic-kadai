41ac2e47ed08860057833a23aca4fdf864a21b58
aiu149 <tianzhongzuoji2@gmail.com>
2024-12-07 06:44:07 +0900

    Update KatoIchiro_Chapter18.java

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -2,14 +2,21 @@ package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
 	
-	public KatoIchiro_Chapter18(String familyName,String givenName,String address) {
-    	super(familyName,givenName,address);
+	public void setFamilyName(String familyName) {
+		
+    	this.familyName = familyName;
 	}
-    public void setGivenName() {
 	
+    public void setGivenName(String givenName) {
+    	
+    	this.givenName = givenName;
 	    System.out.println();
 	
  }
+    public void setAddress(String address) {
+    	this.address = address;
+    	
+    }
 public void eachlntroduce() {
 	System.out.println("好きな食べ物はリンゴです");
 	
