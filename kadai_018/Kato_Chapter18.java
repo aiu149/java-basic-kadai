342ecea9f655b0d1a40e306091f592ca681f026d
aiu149 <tianzhongzuoji2@gmail.com>
2024-12-11 18:42:56 +0900

    Update Kato_Chapter18.java
    変更しました

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -1,23 +1,23 @@
 package kadai_018;
 
 abstract public class Kato_Chapter18 {
-	
-	public String familyName;//姓を表す
+
+	public String familyName = "加藤";//姓を表す
 	public String givenName;//名を表す
-	public String address;//住所を表す
-	
+	public String address = "東京都中野区〇×";//住所を表す
+
 	public void commonlntroduce() {
-		
+
 		System.out.println("私の名前は" + familyName + givenName + "です");
 		System.out.println("住所は" + address + "です");
 	}
-	
+
 	abstract public void eachlntroduce();
-	
+
 	public void execlntroduce() {
 		commonlntroduce();
 		eachlntroduce();
+		System.out.println();
 	}
 
 }
-
