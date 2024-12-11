74ae4d209f0d379f698d15909011217f7f42f988
aiu149 <tianzhongzuoji2@gmail.com>
2024-12-11 18:36:25 +0900

    Update KatoExec_Chapter18.java
    変更しました

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -3,30 +3,19 @@ package kadai_018;
 public class KatoExec_Chapter18 {
 
 	public static void main(String[] args) {
-		
+
 		KatoTaro_Chapter18 a = new KatoTaro_Chapter18();
 		KatoIchiro_Chapter18 b = new KatoIchiro_Chapter18();
 		KatoHanako_Chapter18 c = new KatoHanako_Chapter18();
-		
-		a.setGivenName("太郎");
-		a.setFamilyName("加藤");
-		a.setAddress("東京都中野区");
-		
+
+		a.setGivenName();
 		a.execlntroduce();
-		
-		b.setGivenName("一郎");
-		b.setFamilyName("加藤");
-		b.setAddress("東京都中野区");
-			
+
+		b.setGivenName();
 		b.execlntroduce();
-		
-		c.setGivenName("太郎");
-		c.setFamilyName("加藤");
-		c.setAddress("東京都中野区");
-		
-		
+
+		c.setGivenName();
 		c.execlntroduce();
-		
 
 	}
 
